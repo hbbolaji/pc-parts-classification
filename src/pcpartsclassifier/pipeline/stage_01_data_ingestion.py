4257dadb41accb32459be84f566a5ec9aff99858
HASHIM BOLAJI BELLO <bolaji1050@gmail.com>
2024-03-31 15:11:36 +0300

    pipeline tracking with dvc added

--- src/pcpartsclassifier/pipeline/stage_01_data_ingestion.py
+++ src/pcpartsclassifier/pipeline/stage_01_data_ingestion.py
@@ -13,7 +13,7 @@ class DataIngestionTrainingPipeline:
     config = ConfigurationManager()
     data_ingestion_config = config.get_data_ingestion_config()
     data_ingestion = DataIngestion(config=data_ingestion_config)
-    # data_ingestion.download_data()
+    data_ingestion.download_data()
     data_ingestion.extract_zip_file()
 
 if __name__ == '__main__':
